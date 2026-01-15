e9bfb3d10ff35204e67f728aa38a12e32652191a
Rhiannon <153234674+RCHall01@users.noreply.github.com>
2026-01-14 19:58:07 -0500

    Delete scr directory

--- scr/Land Gen.R
+++
@@ -1,5 +0,0 @@
-
-#Load in Packages
-
-# Load genind object from the saved file
-SNP_genind <- readRDS("C:/Users/Rhall/OneDrive - North Dakota University System/Masters/Thesis Genetics/Practice/outputs/SNP_genind.rds")
--- scr/test.R
+++
@@ -1,5 +0,0 @@
-test
-
-krngkngklerng
-
-nkdnskgnro
